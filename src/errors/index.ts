3ceb0757cc0855a1d8e658bb5163980b4da2e20a
Hosni Bounechada <hosni.bounechada@gmail.com>
2022-09-22 12:13:50 +0100

    not-found-error implementation

--- src/errors/index.ts
+++ src/errors/index.ts
@@ -2,3 +2,4 @@ export * from "./custom-error";
 export * from "./not-authorized-error";
 export * from "./bad-request-error";
 export * from "./request-validation-error";
+export * from "./Not-found-error";
