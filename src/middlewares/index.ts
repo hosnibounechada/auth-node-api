8908cafa8edd1012a6c5eb24332755fd40f6ed3e
Hosni Bounechada <hosni.bounechada@gmail.com>
2022-09-22 12:14:50 +0100

    autherization middleware

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,2 +1,6 @@
+import { requireAuth } from "./require-auth";
+
 export * from "./request-validator";
 export * from "./error-handler";
+export * from "./current-user";
+export * from "./require-auth";
